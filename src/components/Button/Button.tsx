ac5b2e5f179d93c203acd3c6f17c26421cb973f6
Tim <harvard90873@gmail.com>
2022-02-13 20:32:04 +0100

    complete basic button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,13 +16,13 @@ export interface CustomButtonProps {
 }
 
 const COLORS: Record<CSType, string> = {
-  cyan: "bg-cyan-400 hover:bg-cyan-300 focus:bg-cyan-600 text-white",
-  dark: "bg-slate-900 hover:bg-slate-800 focus:bg-black text-white",
-  light: "bg-slate-200 hover:bg-slate-50 focus:bg-slate-400 text-black",
-  orange: "bg-orange-500 hover:bg-orange-400 focus:bg-orange-700 text-white",
+  cyan: "bg-cyan-400 hover:bg-cyan-300 focus:bg-cyan-500 text-white",
+  dark: "bg-slate-800 hover:bg-slate-700 focus:bg-slate-900 text-white",
+  light: "bg-neutral-100 hover:bg-neutral-50 focus:bg-slate-100 text-black",
+  orange: "bg-orange-500 hover:bg-orange-400 focus:bg-orange-600 text-white",
   red: "bg-red-600 hover:bg-red-500 focus:bg-red-700 text-white",
-  sky: "bg-sky-500 hover:bg-sky-400 focus:bg-sky-700 text-white",
-  teal: "bg-teal-400 hover:bg-teal-300 focus:bg-teal-600 text-white",
+  sky: "bg-sky-500 hover:bg-sky-400 focus:bg-sky-600 text-white",
+  teal: "bg-teal-400 hover:bg-teal-300 focus:bg-teal-500 text-white",
   emerald:
     "bg-emerald-500 hover:bg-emerald-400 focus:bg-emerald-600 text-white",
   green: "bg-green-500 hover:bg-green-400 focus:bg-green-600 text-white",
@@ -34,9 +34,9 @@ export const Button: FC<
 > = (props) => {
   return (
     <button
-      className={`w-96 ${
+      className={`px-4 py-2 ${
         COLORS[props.color || "orange"]
-      } rounded-md text-center`}
+      } rounded-md text-center focus:outline-offset-2 focus:outline focus:outline-2 focus:outline-blue-600`}
       {...props}
     ></button>
   );
