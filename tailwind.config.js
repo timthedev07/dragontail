da2fb9e363e8e16243433755844f6becfc653d40
Tim <harvard90873@gmail.com>
2022-02-13 22:04:33 +0100

    damn it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: [
-    {
-      pattern:
-        /(bg|text|border)-(sky|teal|red|orange|dark|light|cyan)-(100|200|300|400|500|600|700|800|900)/,
-    },
-  ],
   theme: {
     extend: {},
   },
