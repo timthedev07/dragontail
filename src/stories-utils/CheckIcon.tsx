61de5401b3de904b757234ad4330e4798d9eb878
Tim <harvard90873@gmail.com>
2022-02-18 18:15:52 +0100

    update icon

--- src/stories-utils/CheckIcon.tsx
+++ src/stories-utils/CheckIcon.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 
-interface CheckIconProps {
-  className?: string;
-}
-
-export const CheckIcon: FC<CheckIconProps> = ({ className }) => {
+export const CheckIcon: FC = ({}) => {
   return (
-    <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      style={{
+        width: "18px",
+        color: "green",
+      }}
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
-        className={className}
+        fill="currentColor"
         d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
       />
     </svg>
