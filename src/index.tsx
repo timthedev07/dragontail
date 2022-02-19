2083cf598a31ed95597ce0b5773dda3efa30e71f
Tim <harvard90873@gmail.com>
2022-02-19 13:28:17 +0100

    export textarea component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import "./dragontail.css";
 export { Button } from "./components/button";
 export { Input } from "./components/input";
+export { Textarea } from "./components/textarea";
 export { DragontailProvider } from "./context/ThemeContext";
