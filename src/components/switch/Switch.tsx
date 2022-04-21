a11203af712fbbe1f2a4f7fa1fdaf4cbdb704fa9
Tim <harvard90873@gmail.com>
2022-04-21 14:50:01 +0200

    fix cursor conflicts

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -98,7 +98,7 @@ export const Switch: FC<SwitchProps> = ({
         }}
         className={`${
           MEASUREMENTS[scale].switchDimensions
-        } cursor-pointer rounded-full appearance-none transition-colors duration-200 ${
+        } rounded-full appearance-none transition-colors duration-200 ${
           theme === "light" ? "bg-neutral-300" : "bg-neutral-500/80"
         } ${CHECKED_COLOR_MAP[checkedColor]} flex items-center ${
           MEASUREMENTS[scale].switchPaddings
@@ -106,7 +106,7 @@ export const Switch: FC<SwitchProps> = ({
           MEASUREMENTS[scale].switchButtonDimensions
         } after:rounded-full after:bg-white after:absolute relative after:transform after:transition after:duration-200 after:ease-out ${
           MEASUREMENTS[scale].switchButtonDisplacement
-        } ${isDisabled && "cursor-not-allowed opacity-40"}`}
+        } ${isDisabled ? "cursor-not-allowed opacity-40" : "cursor-pointer"}`}
         type="checkbox"
       />
     </div>
