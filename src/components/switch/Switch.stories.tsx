66e995d92754b2ea7be1ecbd0e78ec2208a74809
Tim <harvard90873@gmail.com>
2022-04-21 14:50:11 +0200

    more stories

--- src/components/switch/Switch.stories.tsx
+++ src/components/switch/Switch.stories.tsx
@@ -1,5 +1,4 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { useEffect, useState } from "react";
 import { Switch, SwitchProps } from "./Switch";
 
 export default {
@@ -7,11 +6,35 @@ export default {
   component: Switch,
 } as ComponentMeta<typeof Switch>;
 
-const Template: ComponentStory<typeof Switch> = ({ ...args }) => {
-  return <Switch {...args} />;
+const Template: ComponentStory<typeof Switch> = ({ scale: _, ...args }) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <Switch {...args} scale="sm" />
+      <Switch {...args} scale="md" />
+      <Switch {...args} scale="lg" />
+      <Switch {...args} scale="xl" />
+    </div>
+  );
 };
 
-export const Example = Template.bind({});
-Example.args = {
-  // defaultChecked: true,
+export const Examples = Template.bind({});
+
+export const DarkExamples = Template.bind({});
+DarkExamples.args = {
+  theme: "dark",
 } as SwitchProps;
+DarkExamples.parameters = { backgrounds: { default: "dark" } };
+
+export const DefaultChecked = Template.bind({});
+DefaultChecked.args = {
+  defaultChecked: true,
+  theme: "dark",
+} as SwitchProps;
+DefaultChecked.parameters = { backgrounds: { default: "dark" } };
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  isDisabled: true,
+  theme: "dark",
+} as SwitchProps;
+Disabled.parameters = { backgrounds: { default: "dark" } };
