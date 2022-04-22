25126c07787194c9cf0d8f16119ea47759320153
Tim <harvard90873@gmail.com>
2022-04-22 21:21:35 +0200

    more helper functions

--- src/types/Children.tsx
+++ src/types/Children.tsx
@@ -3,3 +3,16 @@ import { ReactNode } from "react";
 export type MaybeRenderProp<PropsType> =
   | ReactNode
   | ((props: PropsType) => React.ReactNode);
+
+export const isFunction = <T extends Function = Function>(
+  value: any
+): value is T => {
+  return typeof value === "function";
+};
+
+export const runIfFn = <T, U>(
+  valueOrFn: T | ((...fnArgs: U[]) => T),
+  ...args: U[]
+): T => {
+  return isFunction(valueOrFn) ? valueOrFn(...args) : valueOrFn;
+};
