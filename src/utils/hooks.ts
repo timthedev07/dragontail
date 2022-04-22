8d62a7217bd133377d6a4d348c4e018f9423c164
Tim <harvard90873@gmail.com>
2022-04-22 15:01:03 +0200

    some helper hooks

---
+++ src/utils/hooks.ts
@@ -0,0 +1,32 @@
+import { MutableRefObject, useEffect, useRef } from "react";
+
+export const useClickOutside = (
+  ref: MutableRefObject<HTMLElement | null>,
+  callback: Function
+) => {
+  useEventListener("click", (e: any) => {
+    if (ref.current == null || ref.current.contains(e.target)) return;
+    callback(e);
+    document;
+  });
+};
+
+export const useEventListener = (
+  eventType: keyof WindowEventMap,
+  callback: Function,
+  element: typeof window | typeof document = window
+) => {
+  const ref = useRef(callback);
+
+  useEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (element == null) return;
+    const handler = (e: any) => ref.current(e);
+    element.addEventListener(eventType, handler);
+
+    return () => element.removeEventListener(eventType, handler);
+  }, [eventType, element]);
+};
