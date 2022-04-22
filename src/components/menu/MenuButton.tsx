c4395381acc375034b67a9a07d4c8eafd8d04795
Tim <harvard90873@gmail.com>
2022-04-22 12:32:02 +0200

    continue with menu button

--- src/components/menu/MenuButton.tsx
+++ src/components/menu/MenuButton.tsx
@@ -12,21 +12,24 @@ export const MenuButton: FC<
 
   return (
     <Button
+      color="neutral"
       onClick={() => {
         setIsOpen((prev) => !prev);
       }}
       {...props}
       rightIcon={
         <svg
-          className="w-[60%] h-[60%]"
+          className="w-[70%] h-[70%]"
           id="Layer_1"
           version="1.1"
+          stroke="currentColor"
+          fill="currentColor"
           viewBox="0 0 512 512"
           xmlSpace="preserve"
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
         >
-          <polygon points="396.6,160 416,180.7 256,352 96,180.7 115.3,160 256,310.5 " />
+          <path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z" />
         </svg>
       }
     />
