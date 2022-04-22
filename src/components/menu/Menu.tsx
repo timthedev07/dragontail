0adcb37832256dd80f4b6619bc1e1cf105e105bd
Tim <harvard90873@gmail.com>
2022-04-22 13:18:50 +0200

    more stuff in menu context

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,22 +1,35 @@
 import React, { Dispatch, useContext, useState } from "react";
+import { DragontailThemeType, useDragontail } from "../../context/ThemeContext";
+
+export interface MenuContextProps {
+  theme?: DragontailThemeType;
+}
 
 interface MenuContextType {
+  theme: DragontailThemeType;
   isOpen: boolean;
   setIsOpen: Dispatch<React.SetStateAction<boolean>>;
 }
+
 const MenuContext = React.createContext<MenuContextType>({
   isOpen: false,
   setIsOpen: () => {},
+  theme: "light",
 });
 
 export const useMenu = () => {
   return useContext(MenuContext);
 };
 
-export const Menu: React.FC = ({ children }) => {
+export const Menu: React.FC<MenuContextProps> = ({ children, theme }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const appTheme = useDragontail();
 
-  const value: MenuContextType = { isOpen, setIsOpen };
+  const value: MenuContextType = {
+    isOpen,
+    setIsOpen,
+    theme: theme || appTheme,
+  };
 
   return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>;
 };
