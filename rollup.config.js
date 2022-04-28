d946f760be27f90a8c15423991b9095c8216f80e
Tim <harvard90873@gmail.com>
2022-04-28 21:03:34 +0200

    fix css injection

--- rollup.config.js
+++ rollup.config.js
@@ -32,10 +32,4 @@ export default [
       terser(),
     ],
   },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
 ];
