806e5e17448917508f7d107c4fc7de0ff79608b6
Tim <harvard90873@gmail.com>
2022-05-06 22:40:58 +0200

    use forwardRef to provide ref functionality
    hope it doesn't break anything

--- src/components/form-control/FormLabel.tsx
+++ src/components/form-control/FormLabel.tsx
@@ -1,24 +1,34 @@
-import { DetailedHTMLProps, FC, LabelHTMLAttributes } from "react";
+import { DetailedHTMLProps, forwardRef, LabelHTMLAttributes } from "react";
 import { TextboxSharedProps } from "../../types/TextboxSharedProps";
 import { useFormControl } from "./FormControl";
 
-export const FormLabel: FC<
-  DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement> &
-    TextboxSharedProps
-> = ({ theme: propsTheme, children, ...props }) => {
-  const { theme, isRequired, isDisabled } = useFormControl("input-label", {
-    theme: propsTheme,
-  });
+export type FormLabelProps = DetailedHTMLProps<
+  LabelHTMLAttributes<HTMLLabelElement>,
+  HTMLLabelElement
+> &
+  TextboxSharedProps;
 
-  return (
-    <label
-      {...props}
-      className={`${
-        theme === "light" ? "text-black" : "text-neutral-50"
-      } text-base ${isDisabled && "opacity-40"}`}
-    >
-      {children}
-      {isRequired ? <span className="text-red-600 text-base">{" *"}</span> : ""}
-    </label>
-  );
-};
+export const FormLabel = forwardRef<HTMLLabelElement, FormLabelProps>(
+  ({ theme: propsTheme, children, ...props }, ref) => {
+    const { theme, isRequired, isDisabled } = useFormControl("input-label", {
+      theme: propsTheme,
+    });
+
+    return (
+      <label
+        {...props}
+        ref={ref}
+        className={`${
+          theme === "light" ? "text-black" : "text-neutral-50"
+        } text-base ${isDisabled && "opacity-40"}`}
+      >
+        {children}
+        {isRequired ? (
+          <span className="text-red-600 text-base">{" *"}</span>
+        ) : (
+          ""
+        )}
+      </label>
+    );
+  }
+);
