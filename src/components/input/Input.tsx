673fb488af7abc17ddf8d834a602badce648dce4
Tim <harvard90873@gmail.com>
2022-05-06 22:41:02 +0200

    use forwardRef to provide ref functionality
    hope it doesn't break anything

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,4 +1,9 @@
-import { DetailedHTMLProps, FC, InputHTMLAttributes, ReactNode } from "react";
+import {
+  DetailedHTMLProps,
+  forwardRef,
+  InputHTMLAttributes,
+  ReactNode,
+} from "react";
 import { DragontailSizeType } from "../../types/Sizes";
 import { TextboxSharedProps } from "../../types/TextboxSharedProps";
 import { useFormControl } from "../form-control/FormControl";
@@ -25,83 +30,91 @@ export interface CustomInputProps
   rightElement?: ReactNode;
 }
 
-export const Input: FC<CustomInputProps> = ({
-  children,
-  leftAddon,
-  rightAddon,
-  variant: propsVariant = "solid",
-  color,
-  theme: propsTheme,
-  className,
-  scale: size = "md",
-  leftElement,
-  rightElement,
-  isDisabled: disabled = false,
-  isInvalid: invalid = false,
-  isRequired: required = false,
-  ...props
-}) => {
-  const { isDisabled, isInvalid, theme, variant } = useFormControl(
-    "input-field",
+export const Input = forwardRef<HTMLInputElement, CustomInputProps>(
+  (
     {
-      isDisabled: disabled,
-      isInvalid: invalid,
-      isRequired: required,
+      children,
+      leftAddon,
+      rightAddon,
+      variant: propsVariant = "solid",
+      color,
       theme: propsTheme,
-      variant: propsVariant,
-    }
-  );
-  const ADDON_CLASS = `p-2 flex justify-center items-center ${
-    INPUT_SIZES[size]
-  } ${
-    theme === "dark" ? "bg-slate-600 text-white" : "bg-slate-200 text-slate-700"
-  }`;
+      className,
+      scale: size = "md",
+      leftElement,
+      rightElement,
+      isDisabled: disabled = false,
+      isInvalid: invalid = false,
+      isRequired: required = false,
+      ...props
+    },
+    ref
+  ) => {
+    const { isDisabled, isInvalid, theme, variant } = useFormControl(
+      "input-field",
+      {
+        isDisabled: disabled,
+        isInvalid: invalid,
+        isRequired: required,
+        theme: propsTheme,
+        variant: propsVariant,
+      }
+    );
+    const ADDON_CLASS = `p-2 flex justify-center items-center ${
+      INPUT_SIZES[size]
+    } ${
+      theme === "dark"
+        ? "bg-slate-600 text-white"
+        : "bg-slate-200 text-slate-700"
+    }`;
 
-  const ELEMENT_CLASS = `absolute ${
-    INPUT_SIZES[size]
-  } w-10 font-sans flex justify-center items-center bg-inherit ${
-    theme === "dark" ? "text-white" : "text-slate-700"
-  }`;
+    const ELEMENT_CLASS = `absolute ${
+      INPUT_SIZES[size]
+    } w-10 font-sans flex justify-center items-center bg-inherit ${
+      theme === "dark" ? "text-white" : "text-slate-700"
+    }`;
 
-  return (
-    <div className="relative flex justify-center items-center">
-      {leftAddon ? (
-        <div className={`${ADDON_CLASS} rounded-l-md`}>{leftAddon}</div>
-      ) : null}
-      {leftElement ? (
-        <div className={`${ELEMENT_CLASS} left-0`}>{leftElement}</div>
-      ) : null}
-      <input
-        className={`${className || ""} ${disabledClasses(isDisabled)} ${
-          leftElement && rightElement
-            ? "px-10"
-            : leftElement
-            ? "pl-10 pr-3"
-            : rightElement
-            ? "pr-10 pl-3"
-            : INPUT_DEFAULT_PADDING[variant]
-        } ${invalidClasses(isInvalid, theme, variant === "solid")} ${
-          INPUT_BASE[theme][variant]
-        } ${INPUT_SIZES[size]} ${INPUT_VARIANTS_BORDER[variant]} ${
-          INPUT_CORNER_ROUNDING[variant][
-            leftAddon && rightAddon
-              ? "none"
-              : leftAddon
-              ? "right"
-              : rightAddon
-              ? "left"
-              : "all"
-          ]
-        }`}
-        {...props}
-        disabled={isDisabled}
-      />
-      {rightElement ? (
-        <div className={`${ELEMENT_CLASS} right-0`}>{rightElement}</div>
-      ) : null}
-      {rightAddon ? (
-        <div className={`${ADDON_CLASS} rounded-r-md`}>{rightAddon}</div>
-      ) : null}
-    </div>
-  );
-};
+    return (
+      <div className="relative flex justify-center items-center">
+        {leftAddon ? (
+          <div className={`${ADDON_CLASS} rounded-l-md`}>{leftAddon}</div>
+        ) : null}
+        {leftElement ? (
+          <div className={`${ELEMENT_CLASS} left-0`}>{leftElement}</div>
+        ) : null}
+        <input
+          ref={ref}
+          className={`${className || ""} ${disabledClasses(isDisabled)} ${
+            leftElement && rightElement
+              ? "px-10"
+              : leftElement
+              ? "pl-10 pr-3"
+              : rightElement
+              ? "pr-10 pl-3"
+              : INPUT_DEFAULT_PADDING[variant]
+          } ${invalidClasses(isInvalid, theme, variant === "solid")} ${
+            INPUT_BASE[theme][variant]
+          } ${INPUT_SIZES[size]} ${INPUT_VARIANTS_BORDER[variant]} ${
+            INPUT_CORNER_ROUNDING[variant][
+              leftAddon && rightAddon
+                ? "none"
+                : leftAddon
+                ? "right"
+                : rightAddon
+                ? "left"
+                : "all"
+            ]
+          }`}
+          {...props}
+          disabled={isDisabled}
+        />
+        {rightElement ? (
+          <div className={`${ELEMENT_CLASS} right-0`}>{rightElement}</div>
+        ) : null}
+        {rightAddon ? (
+          <div className={`${ADDON_CLASS} rounded-r-md`}>{rightAddon}</div>
+        ) : null}
+      </div>
+    );
+  }
+);
