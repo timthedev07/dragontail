368291ff8a31687e78b13c9b1420096814ffcdbd
Tim <harvard90873@gmail.com>
2022-05-06 22:41:06 +0200

    use forwardRef to provide ref functionality
    hope it doesn't break anything

--- src/components/input/PasswordInput.tsx
+++ src/components/input/PasswordInput.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { forwardRef, useState } from "react";
 import { Button } from "../button";
 import { useFormControl } from "../form-control/FormControl";
 import { CustomInputProps } from "./Input";
@@ -12,63 +12,69 @@ import {
   invalidClasses,
 } from "./styles";
 
-export const PasswordInput: FC<CustomInputProps> = ({
-  children,
-  leftAddon,
-  rightAddon,
-  color,
-  theme: propsTheme,
-  variant: propsVariant = "solid",
-  isInvalid: invalid = false,
-  isDisabled: disabled = false,
-  isRequired: required = false,
-  className,
-  scale: size = "md",
-  ...props
-}) => {
-  const { isDisabled, isInvalid, theme, variant } = useFormControl(
-    "input-field",
+export const PasswordInput = forwardRef<HTMLInputElement, CustomInputProps>(
+  (
     {
-      isDisabled: disabled,
-      isInvalid: invalid,
-      isRequired: required,
+      children,
+      leftAddon,
+      rightAddon,
+      color,
       theme: propsTheme,
-      variant: propsVariant,
-    }
-  );
-  const [showPw, setShowPw] = useState<boolean>(false);
+      variant: propsVariant = "solid",
+      isInvalid: invalid = false,
+      isDisabled: disabled = false,
+      isRequired: required = false,
+      className,
+      scale: size = "md",
+      ...props
+    },
+    ref
+  ) => {
+    const { isDisabled, isInvalid, theme, variant } = useFormControl(
+      "input-field",
+      {
+        isDisabled: disabled,
+        isInvalid: invalid,
+        isRequired: required,
+        theme: propsTheme,
+        variant: propsVariant,
+      }
+    );
+    const [showPw, setShowPw] = useState<boolean>(false);
 
-  return (
-    <div className="relative flex justify-end">
-      <input
-        {...props}
-        className={`${className} ${disabledClasses(
-          isDisabled
-        )} ${invalidClasses(isInvalid, theme)} ${
-          INPUT_DEFAULT_PADDING[variant]
-        } ${INPUT_CORNER_ROUNDING[variant]["all"]} ${
-          INPUT_BASE[theme][variant]
-        } relative ${INPUT_SIZES[size]} pr-20 ${
-          INPUT_VARIANTS_BORDER[variant]
-        }`}
-        type={showPw ? "text" : "password"}
-        disabled={isDisabled}
-      />
-      <div
-        className={`absolute flex justify-center items-center px-2 ${INPUT_SIZES[size]} w-20`}
-      >
-        <Button
-          color="neutral"
-          theme={theme}
-          className="transition h-4/6 px-3 w-full"
-          onClick={() => {
-            setShowPw((prev) => !prev);
-          }}
-          focusEffect={false}
+    return (
+      <div className="relative flex justify-end">
+        <input
+          ref={ref}
+          {...props}
+          className={`${className} ${disabledClasses(
+            isDisabled
+          )} ${invalidClasses(isInvalid, theme)} ${
+            INPUT_DEFAULT_PADDING[variant]
+          } ${INPUT_CORNER_ROUNDING[variant]["all"]} ${
+            INPUT_BASE[theme][variant]
+          } relative ${INPUT_SIZES[size]} pr-20 ${
+            INPUT_VARIANTS_BORDER[variant]
+          }`}
+          type={showPw ? "text" : "password"}
+          disabled={isDisabled}
+        />
+        <div
+          className={`absolute flex justify-center items-center px-2 ${INPUT_SIZES[size]} w-20`}
         >
-          {showPw ? "Hide" : "Show"}
-        </Button>
+          <Button
+            color="neutral"
+            theme={theme}
+            className="transition h-4/6 px-3 w-full"
+            onClick={() => {
+              setShowPw((prev) => !prev);
+            }}
+            focusEffect={false}
+          >
+            {showPw ? "Hide" : "Show"}
+          </Button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
