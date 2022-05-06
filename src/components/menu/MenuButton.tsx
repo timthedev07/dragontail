c11990e536f7023f2b94764a863b80c15e460db4
Tim <harvard90873@gmail.com>
2022-05-06 22:41:11 +0200

    use forwardRef to provide ref functionality
    hope it doesn't break anything

--- src/components/menu/MenuButton.tsx
+++ src/components/menu/MenuButton.tsx
@@ -1,42 +1,45 @@
-import { ClassAttributes, FC, HTMLAttributes } from "react";
+import { ClassAttributes, forwardRef, HTMLAttributes } from "react";
 import { Button } from "../button";
 import { CustomButtonProps } from "../button/Button";
 import { useMenu } from "./Menu";
 
-export const MenuButton: FC<
-  ClassAttributes<HTMLButtonElement> &
-    HTMLAttributes<HTMLButtonElement> &
-    CustomButtonProps
-> = (props) => {
-  const { close, open, isOpen, theme } = useMenu();
+export type MenuButtonType = ClassAttributes<HTMLButtonElement> &
+  HTMLAttributes<HTMLButtonElement> &
+  CustomButtonProps;
 
-  return (
-    <Button
-      color="neutral"
-      theme={theme}
-      onClick={() => {
-        if (isOpen) {
-          close();
-        } else {
-          open();
+export const MenuButton = forwardRef<HTMLButtonElement, MenuButtonType>(
+  ({ ref: _, ...props }, ref) => {
+    const { close, open, isOpen, theme } = useMenu();
+
+    return (
+      <Button
+        color="neutral"
+        theme={theme}
+        ref={ref}
+        onClick={() => {
+          if (isOpen) {
+            close();
+          } else {
+            open();
+          }
+        }}
+        {...props}
+        rightIcon={
+          <svg
+            className="w-[70%] h-[70%]"
+            id="Layer_1"
+            version="1.1"
+            stroke="currentColor"
+            fill="currentColor"
+            viewBox="0 0 512 512"
+            xmlSpace="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+          >
+            <path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z" />
+          </svg>
         }
-      }}
-      {...props}
-      rightIcon={
-        <svg
-          className="w-[70%] h-[70%]"
-          id="Layer_1"
-          version="1.1"
-          stroke="currentColor"
-          fill="currentColor"
-          viewBox="0 0 512 512"
-          xmlSpace="preserve"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z" />
-        </svg>
-      }
-    />
-  );
-};
+      />
+    );
+  }
+);
