8cc90e6702de557919a1051c0f9d9877c66b7568
Tim <harvard90873@gmail.com>
2022-05-06 22:41:26 +0200

    use forwardRef to provide ref functionality
    hope it doesn't break anything

--- src/components/textarea/TextArea.tsx
+++ src/components/textarea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { FC, DetailedHTMLProps, TextareaHTMLAttributes } from "react";
+import { DetailedHTMLProps, TextareaHTMLAttributes, forwardRef } from "react";
 import { TextboxSharedProps } from "../../types/TextboxSharedProps";
 import { useFormControl } from "../form-control/FormControl";
 import {
@@ -27,38 +27,44 @@ const RESIZE: Record<ResizeType, string> = {
   both: "resize",
 };
 
-export const Textarea: FC<TextAreaProps> = ({
-  variant: propsVariant = "solid",
-  className = "",
-  theme: propsTheme,
-  isDisabled: disabled = false,
-  isInvalid: invalid = false,
-  isRequired: required = false,
-  resize = "none",
-  ...props
-}) => {
-  const { isDisabled, isInvalid, theme, variant } = useFormControl(
-    "input-field",
+export const Textarea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
     {
-      isDisabled: disabled,
-      isInvalid: invalid,
-      isRequired: required,
+      variant: propsVariant = "solid",
+      className = "",
       theme: propsTheme,
-      variant: propsVariant,
-    }
-  );
+      isDisabled: disabled = false,
+      isInvalid: invalid = false,
+      isRequired: required = false,
+      resize = "none",
+      ...props
+    },
+    ref
+  ) => {
+    const { isDisabled, isInvalid, theme, variant } = useFormControl(
+      "input-field",
+      {
+        isDisabled: disabled,
+        isInvalid: invalid,
+        isRequired: required,
+        theme: propsTheme,
+        variant: propsVariant,
+      }
+    );
 
-  return (
-    <textarea
-      {...props}
-      disabled={isDisabled}
-      className={`${className} ${RESIZE[resize]} ${disabledClasses(
-        isDisabled
-      )} ${invalidClasses(isInvalid, theme, variant === "solid")} ${
-        INPUT_DEFAULT_PADDING[variant]
-      } ${INPUT_CORNER_ROUNDING[variant]["all"]} ${
-        TEXTAREA_VARIANTS[variant]
-      } ${TEXTAREA_BASE[theme][variant]} ${RESIZE[resize]}`}
-    ></textarea>
-  );
-};
+    return (
+      <textarea
+        ref={ref}
+        {...props}
+        disabled={isDisabled}
+        className={`${className} ${RESIZE[resize]} ${disabledClasses(
+          isDisabled
+        )} ${invalidClasses(isInvalid, theme, variant === "solid")} ${
+          INPUT_DEFAULT_PADDING[variant]
+        } ${INPUT_CORNER_ROUNDING[variant]["all"]} ${
+          TEXTAREA_VARIANTS[variant]
+        } ${TEXTAREA_BASE[theme][variant]} ${RESIZE[resize]}`}
+      ></textarea>
+    );
+  }
+);
