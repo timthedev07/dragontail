cedc54f593e8736288c096368602f2e4c9ccb4d0
Tim <harvard90873@gmail.com>
2022-05-15 15:23:59 +0200

    fix variants not being bundled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,39 @@
+const plugin = require(`tailwindcss/plugin`);
+
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       keyframes: {
-        "menu-open": {
-          "0%": {
-            opacity: "0",
-            transform: "scale(0.9)",
+        'menu-open': {
+          '0%': {
+            opacity: '0',
+            transform: 'scale(0.9)',
           },
-          "100%": {
-            opacity: "1",
-            transform: "scale(1)",
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1)',
           },
         },
       },
       animation: {
-        "menu-open": "menu-open 200ms ease-in-out",
+        'menu-open': 'menu-open 200ms ease-in-out',
       },
     },
   },
   variants: {
     extend: {
-      backgroundColor: ["checked"],
+      backgroundColor: ['checked'],
     },
   },
   plugins: [
-    ({ addVariant }) => {
-      addVariant("hocus", ["&:hover", "&:focus"]);
-      addVariant("child-list", "& ol", "& ul");
-      addVariant("child-code", "& pre");
-    },
+    plugin(
+        ({addVariant}) =>
+        {
+          addVariant('hocus', ['&:hover', '&:focus']);
+          addVariant('child-list', '& ol', '& ul');
+          addVariant('child-code', '& pre');
+        }),
   ],
 };
