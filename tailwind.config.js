0bf05e7b661b0acea1a9776378eb13b48352b846
Tim <harvard90873@gmail.com>
2023-04-28 00:17:37 +0200

    smooooooth toast animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,37 @@ module.exports = {
             transform: "scale(1)",
           },
         },
+        "toast-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(200px)",
+          },
+          "70%": {
+            opacity: "0.4",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "toast-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-200px)",
+          },
+          "70%": {
+            opacity: "0.4",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
       animation: {
         "menu-open": "menu-open 200ms ease-in-out",
+        "toast-up": "toast-up 500ms cubic-bezier(0,-0.01,0,.99)",
+        "toast-down": "toast-down 500ms cubic-bezier(0,-0.01,0,.99)",
       },
     },
   },
